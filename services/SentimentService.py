6dfa362002277618aa2cc0571c69520eab579968
sittichok Ouamsiri <sutichok46@gmail.com>
2024-05-02 01:05:26 +0700

    feat: add sentiment

--- services/SentimentService.py
+++ services/SentimentService.py
@@ -5,6 +5,7 @@ import pickle
 from services.NLPService import clean, extractOrg
 from services.DBService import searchTweetByMultipleOrgs, listToDictEmptyArray, searchTweetContainProductAndOrg
 
+
 def getSentimentAnalysis(tweet):
     orgList = extractOrg(clean(tweet))
     if orgList == None:
@@ -16,12 +17,13 @@ def getSentimentAnalysis(tweet):
     for org in orgsWithTweets:
         data = {}
         sentiments = analyze(orgsWithTweets[org])
-        mean = average(sentiments)
+        mean = fuzzyLogic(average(sentiments))
         data['brand'] = org
         data['sentiment'] = mean
         data['examples'] = sentiments[:2]
         result.append(data)
     return result
+
 
 # receive list of tweets from an org
 def analyze(tweets):
@@ -36,6 +38,7 @@ def analyze(tweets):
         result.append(data)
     return result
 
+
 def findOrgInTweets(tweets):
     orgSet = set()
     for tweet in tweets:
@@ -46,6 +49,7 @@ def findOrgInTweets(tweets):
     dict = listToDictEmptyArray(list(orgSet))
     return dict
 
+
 # receive list of sentiment score
 def average(sentiments):
     sum = 0.0
@@ -53,10 +57,64 @@ def average(sentiments):
         sum += sentiments[i]['sentiment']
     return sum / len(sentiments)
 
+
 def predictData(text):
-    with open('resources/model.pkl','rb') as f:
+    with open('resources/model.pkl', 'rb') as f:
         clf2 = pickle.load(f)
 
     text = clean(text)
     data = clf2.predict([text])
     return data[0]
+
+
+def fuzzyLogic(sentiments):
+    fuzzy = {
+        'angry': 0,
+        'sad': 0,
+        'neutral': 0,
+        'happy': 0,
+        'excited': 0
+    }
+    # < -6 Angry
+    if sentiments <= -6:
+        fuzzy['angry'] += 1
+    # -5 - -2 Sad
+    if sentiments > -5 and sentiments < -2:
+        fuzzy['sad'] += sentiments + 5 / 3
+
+    if sentiments == -2:
+        fuzzy['sad'] += 1
+
+    if sentiments > -2 and sentiments < 0:
+        fuzzy['sad'] += abs(sentiments) / 2
+
+    # 0 Neutral
+
+    if sentiments > -1 and sentiments < 0:
+        fuzzy['neutral'] += abs(sentiments - 1)
+
+    if sentiments == 0:
+        fuzzy['neutral'] += 1
+
+    if sentiments > 0 and sentiments < 1:
+        fuzzy['neutral'] += sentiments
+    # 0 - 3 Happy
+    if sentiments > 0 and sentiments < 2:
+        fuzzy['happy'] += sentiments / 2
+
+    if sentiments == 2:
+        fuzzy['happy'] += 1
+
+    if sentiments > 2 and sentiments < 5:
+        fuzzy['happy'] += sentiments / 5
+    # > 3 Excited
+    if sentiments > 5 and sentiments < 6:
+        fuzzy['excited'] += sentiments - 5
+
+    response = ""
+
+    for key in fuzzy:
+        if fuzzy[key] > 0:
+            response += key + ": " + str(fuzzy[key]) + ", "
+
+    return response
